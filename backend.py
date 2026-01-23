3f30fc85e35cb22e61d275d655f917da3c54021e
marthapavan2002@gmail.com <marthapavan2002@gmail.com>
2026-01-23 15:11:46 +0530

    Added Dockerfile and added steps in deployment.yaml

--- backend.py
+++ backend.py
@@ -54,4 +54,4 @@ def predict():
        return jsonify({'error': str(e), 'type': str(type(e))}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5003, debug=True)
+    app.run(host='0.0.0.0', port=5003, debug=True)
